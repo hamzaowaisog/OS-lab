f5ee9796e66a0dd51260b08d2536b6581b266b53
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-02-22 08:36:47 +0500

    updated Lab 4

--- Lab 4/Task5.c
+++ Lab 4/Task5.c
@@ -14,20 +14,20 @@ int main(){
 
     if(pid1 == 0){
         printf("Child process 1 id = %d with parent process id = %d\n",getpid(),getppid());
-        exit(0);
+
     }
     else if(pid1 > 0){
         printf("Parent process id = %d of child process 1 \n",getpid());
-        exit(0);
+       
     }
     if(pid2 > 0){
         printf("parent process of id = %d of child process 2 id = %d \n",getppid(),getpid());
-        exit(0);
+	exit(0);
+        
     }
     if(pid2 == 0){
+	sleep(10);
         printf("Child process 2 id - %d of parent process id = %d\n",getpid(),getppid());
-        sleep(5);
-        exit(0);
-    }
+         }
     
 }
